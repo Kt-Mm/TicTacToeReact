a6b8c28240a88f227bd4cc091d54e63f3cf14ef6
Kt-Mm <ktine.celmlo@gmail.com>
2023-07-12 00:35:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'; //importing state from react
 
-function App() {
+function Square({ value, onSquareClick }) { //square component that holds value,prop=onSquareClick
+                                            //tells board what to display when the square box is clicked
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) { //component name board that is passing props{xIsNext, squares, onPlay}
+                                              //through other functions calling on board
+  function handleClick(i) { //handleClick component passes through array index(square index) to determine thr winner.
+    if (calculateWinner(squares) || squares[i]) {//If statement to calc.winner passing through squares or the squares index
+      return; //to return if:
+    }
+    const nextSquares = squares.slice(); //variable mnextsquare hold value of squares calling slice method
+                                        // create copy of square array
+    if (xIsNext) { //calling on xIsNext thru the board
+      nextSquares[i] = 'X'; // giving the nextSquare varible index value of X
+    } else { // if xIsNOTNEXT nextSquare varible index is O
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);//varible winner with value of calc.winner passin through squares
+  let status;
+  if (winner) { // if statement for winner bein true then:
+    status = 'Winner: ' + winner; // the status will show winner
+  } else {// if winner not true then:
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O'); // the status will show a conditional statement if
+                                                      //the next player will be x or o
+  }
+
+  return ( // what will be displayed on browser| layout of game board
+    <>
+    
+      <div className="status">{status}</div> 
+      <div className="board-row"> 
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);//holds value of where xisnext. setXNextIs is the changeable state of xIsNext
+  const [history, setHistory] = useState([Array(9).fill(null)]);//holds value of where history. setHistory is the changeable state of history.
+                                                                //iterates through array starting with null to find the history 
+  const currentSquares = history[history.length - 1];// iterates through history length minus 1 to find current square
+
+  function handlePlay(nextSquares) { //handlePlay component handles how game is Played
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => { //iterating through history using mao method
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) { //caluc. the winner by:
+  const lines = [// if the player lands on any one of these square patterens
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) { //looping through the lines length to determine who is the winner
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { // if statement to return which squares are the winner
+                                                                                // by giving  squares a.b,c values of lines index.
+      return squares[a]; // return square a if, if statements are true
+    }
+  }
+  return null; // else return undefined.
+}
+  
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Lines 9-13 brings all the necessary pieces together:
+// React
+// React’s library to talk to web browsers (React DOM)
+// the styles for your components
+// the component you created in App.js.
+// The remainder of the file brings all the pieces together 
+// and injects the final product into index.html in the public folder.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
